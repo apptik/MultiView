501eaa1edab4a8c31e76135731bf99480fe3127c
Kalin Maldzhanski <djodjo@djodjo.org>
2015-09-13 01:41:10 +0200

    refactor scrollers arch

--- scrollers/src/main/java/io/apptik/widget/multiview/scrollers/DefaultSmoothScroller.java
+++ scrollers/src/main/java/io/apptik/widget/multiview/scrollers/DefaultSmoothScroller.java
@@ -19,18 +19,11 @@ package io.apptik.widget.multiview.scrollers;
 
 import android.content.Context;
 import android.graphics.PointF;
-import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 
-public class DefaultSmoothScroller extends LinearSmoothScroller {
+public class DefaultSmoothScroller extends FlexiSmoothScroller {
 
-    public static final String TAG = DefaultSmoothScroller.class.getName();
-
-    public static final int SNAP_START = LinearSmoothScroller.SNAP_TO_START;
-    public static final int SNAP_END = LinearSmoothScroller.SNAP_TO_END;;
-    public static final int SNAP_CENTER = 3;
-    public static final int SNAP_NONE = LinearSmoothScroller.SNAP_TO_ANY;
     public static final float DEFAULT_MILLISECONDS_PER_INCH = 50f;
 
     private float millisecondsPerInch = DEFAULT_MILLISECONDS_PER_INCH;
---
+++ scrollers/src/main/java/io/apptik/widget/multiview/scrollers/FlexiSmoothScroller.java
@@ -0,0 +1,11 @@
+package io.apptik.widget.multiview.scrollers;
+
+
+import android.content.Context;
+
+public abstract class FlexiSmoothScroller extends BaseSmoothScroller {
+
+    public FlexiSmoothScroller(Context context) {
+        super(context);
+    }
+}
