bf3bdafee8da03c8a826e4ade348d17c8cf0c37a
Kalin Maldzhanski <djodjo@djodjo.org>
2015-09-13 11:19:30 +0200

    add scroller before after action

--- scrollers/src/main/java/io/apptik/widget/multiview/scrollers/FlexiSmoothScroller.java
+++ scrollers/src/main/java/io/apptik/widget/multiview/scrollers/FlexiSmoothScroller.java
@@ -7,8 +7,43 @@ import android.support.v7.widget.RecyclerView;
 
 public abstract class FlexiSmoothScroller extends BaseSmoothScroller {
 
+    private Runnable beforeScrollAction;
+    private Runnable afterScrollAction;
+
     public FlexiSmoothScroller(Context context) {
         super(context);
+    }
+
+    public Runnable getAfterScrollAction() {
+        return afterScrollAction;
+    }
+
+    public void setAfterScrollAction(Runnable afterScrollAction) {
+        this.afterScrollAction = afterScrollAction;
+    }
+
+    public Runnable getBeforeScrollAction() {
+        return beforeScrollAction;
+    }
+
+    public void setBeforeScrollAction(Runnable beforeScrollAction) {
+        this.beforeScrollAction = beforeScrollAction;
+    }
+
+    @Override
+    protected void onStop() {
+        if(afterScrollAction!=null) {
+            afterScrollAction.run();
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        if(beforeScrollAction!=null) {
+            beforeScrollAction.run();
+        }
+        super.onStart();
     }
 
     @Override
